a8c56c6b1c1bafcbcc5213bbcf1228251d3d9d29
JongMany <blackberry1114@naver.com>
2024-08-12 07:45:56 +0900

    fix: eslint 옵션 수정(any 타입 허용 및 exhaustive deps 문제)

--- frontend/react-app/eslint.config.mjs
+++ frontend/react-app/eslint.config.mjs
@@ -2,6 +2,9 @@ import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import path from "path";
 import { fileURLToPath } from "url";
+// import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+// import eslintPluginReact from "eslint-plugin-react";
+// import { fixupPluginRules } from "@eslint/compat";
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +15,10 @@ const compat = new FlatCompat({
   resolvePluginsRelativeTo: __dirname, // optional
   recommendedConfig: js.configs.recommended, // optional unless you're using "eslint:recommended"
   allConfig: js.configs.all, // optional unless you're using "eslint:all"
+  // plugins: {
+  //   react: eslintPluginReact,
+  //   "react-hooks": fixupPluginRules(eslintPluginReactHooks),
+  // },
 });
 
 export default [
@@ -33,6 +40,8 @@ export default [
           "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
           "no-unused-vars": "off",
           "@typescript-eslint/no-unused-vars": "warn",
+          "react-hooks/exhaustive-deps": "off",
+          "@typescript-eslint/no-explicit-any": "off",
         },
       },
     ],
@@ -42,3 +51,54 @@ export default [
     node: true,
   }),
 ];
+// import { FlatCompat } from "@eslint/eslintrc";
+// import js from "@eslint/js";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import { fixupPluginRules } from "@eslint/compat";
+// import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+// import eslintPluginReact from "eslint-plugin-react";
+
+// // mimic CommonJS variables -- not needed if using CommonJS
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname, // optional; default: process.cwd()
+//   resolvePluginsRelativeTo: __dirname, // optional
+//   recommendedConfig: js.configs.recommended, // optional unless you're using "eslint:recommended"
+//   allConfig: js.configs.all, // optional unless you're using "eslint:all"
+// });
+
+// export default [
+//   ...compat.config({
+//     root: true,
+//     extends: [
+//       "eslint:recommended",
+//       "plugin:@typescript-eslint/recommended",
+//       "plugin:react-hooks/recommended",
+//     ],
+//     ignorePatterns: ["dist", ".eslintrc.cjs"],
+//     parser: "@typescript-eslint/parser",
+//     // plugins: ["react-refresh", "@typescript-eslint/eslint-plugin"],
+//     plugins: {
+//       react: eslintPluginReact,
+//       "react-hooks": fixupPluginRules(eslintPluginReactHooks),
+//     },
+
+//     overrides: [
+//       {
+//         files: ["**/*.ts", "**/*.tsx", "**/*.jsx", "**/*.js"],
+//         rules: {
+//           "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+//           "no-unused-vars": "off",
+//           "@typescript-eslint/no-unused-vars": "warn",
+//         },
+//       },
+//     ],
+//   }),
+//   ...compat.env({
+//     es2020: true,
+//     node: true,
+//   }),
+// ];
