d69e331f88f261414729f0f2eb24b58c68f8c6f4
nuyeo <bluelayzxx@gmail.com>
2024-08-12 17:36:28 +0900

    feat: 환경변수 설정을 위한 ConfigModule 추가

--- backend/nest-backend/src/app.module.ts
+++ backend/nest-backend/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전역 모듈로 설정
+      // validationSchema,
+      load: [],
+      cache: true,
+      envFilePath: [
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+      ],
+    }),
+    // TypeOrmModule.forRootAsync({
+    //   inject: [ConfigService],
+    //   useFactory: async (configService: ConfigService) =>
+    //     await typeOrmConfig(configService),
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+// function typeOrmConfig(
+//   configService: ConfigService<Record<string, unknown>, false>,
+// ): any {
+//   throw new Error('Function not implemented.');
+// }
