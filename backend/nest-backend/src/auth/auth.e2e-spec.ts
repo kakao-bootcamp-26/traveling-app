565ad31045b7d0049ee4d7c48e726680f2e25357
nuyeo <bluelayzxx@gmail.com>
2024-08-17 16:04:00 +0900

    chore: 백엔드 start:dev script 수정

---
+++ backend/nest-backend/src/auth/auth.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../app.module';
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/google (GET)', () => {
+    return request(app.getHttpServer()).get('/auth/google').expect(302); // Expecting a redirection to Google
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
