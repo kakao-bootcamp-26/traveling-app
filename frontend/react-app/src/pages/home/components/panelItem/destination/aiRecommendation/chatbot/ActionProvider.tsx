411967e145f35c7694995fcbfb0d7e492a8b45ba
JongMany <blackberry1114@naver.com>
2024-08-18 12:17:40 +0900

    refactor: 챗봇 기반 추천 시스템 페이지의 컴포넌트 설계 변경

--- frontend/react-app/src/pages/home/components/panelItem/destination/aiRecommendation/chatbot/ActionProvider.tsx
+++ frontend/react-app/src/pages/home/components/panelItem/destination/aiRecommendation/chatbot/ActionProvider.tsx
@@ -1,19 +1,19 @@
-import { ChatBotProps } from "@/pages/home/components/panelItem/destination/aiRecommendation/chatbot/ChatBot";
-import { selectedTravelInfoSelector } from "@/shared/atom/travelAtom";
+import React from "react";
+import type { PropsWithChildren } from "react";
+import { ChatBotProps } from "@/pages/home/components/panelItem/destination/aiRecommendation/chatbot/ChatBotPage";
 import { DestinationEntity } from "@/shared/entities/destination.entity";
-import React, { PropsWithChildren } from "react";
-import { useSetRecoilState } from "recoil";
 
 type Props = {
   setState: any;
   createChatBotMessage: any;
 };
 type NavigateProps = {
-  moveToResultStep: () => void;
+  moveToAIResultStep: () => void;
   saveAIRecommendationDestinations: (cities: DestinationEntity[]) => void;
 };
+
 const ActionProvider: ChatBotProps["actionProvider"] =
-  ({ moveToResultStep, saveAIRecommendationDestinations }: NavigateProps) =>
+  ({ moveToAIResultStep, saveAIRecommendationDestinations }: NavigateProps) =>
   ({ createChatBotMessage, setState, children }: PropsWithChildren<Props>) => {
     const handleMessage = (message: string) => {
       const messageLower = message.toLowerCase();
@@ -34,31 +34,60 @@ const ActionProvider: ChatBotProps["actionProvider"] =
       const goToNextStep = () => {
         console.log("goToNextStep");
         saveAIRecommendationDestinations(cityCuration);
-        moveToResultStep();
+        moveToAIResultStep();
       };
 
-      const botMessage = createChatBotMessage([
+      const goToPrevStep = () => {
+        console.log("goToPrevStep");
+        const feedbackMessage = createChatBotMessage("피드백이 완료되었습니다.");
+        setState((prev: any) => ({
+          ...prev,
+          messages: [...prev.messages, feedbackMessage],
+        }));
+      };
+
+      const botMessage = createChatBotMessage(
         <>
           <p className="mb-2">후보를 추천해드리겠습니다.</p>
           {cityCuration.map((city, idx) => (
-            <div className="mb-2" key={city.city}>
-              <p>{idx + 1}번째 추천</p>
-              <p>{city.country}</p>
-              <p>{city.city}</p>
-              <p>{city.airport.name}</p>
-              <p>{city.airport.code}</p>
+            <div className="mb-3" key={city.city}>
+              <p className="mb-1">{idx + 1}번째 추천</p>
+              <div className="grid grid-cols-[1fr_2fr]">
+                <p>국가</p>
+                <p>{city.country}</p>
+                <p>도시</p>
+                <p>{city.city}</p>
+                <p>공항</p>
+                <p>
+                  {city.airport.name} ({city.airport.code})
+                </p>
+              </div>
             </div>
           ))}
+          <hr className="mb-2" />
           <div className="mb-2">
-            <p>AI의 추천이 만족스러우셨나요?</p>
+            <p>AI의 추천이 만족스러우셨나요? </p>
+            <p>만족스러우셨다면 "예" 버튼을 눌러주세요</p>
           </div>
           <div className="mb-2">
-            <button onClick={goToNextStep}>예</button>
-            <button>아니요</button>
+            <button
+              className="w-[70px] rounded-md mr-2 px-2 py-1"
+              style={{ border: "1px solid #ffffff" }}
+              onClick={goToNextStep}
+            >
+              예
+            </button>
+            <button
+              className="w-[70px] rounded-md px-2 py-1"
+              style={{ border: "1px solid #ffffff" }}
+              onClick={goToPrevStep}
+            >
+              아니요
+            </button>
           </div>
-          <p>만약 다른 도시를 추천받고 싶으시다면 채팅을 계속 해주세요.</p>
+          <p>다른 도시를 추천받고 싶으시다면 채팅을 계속 해주세요.</p>
         </>,
-      ]);
+      );
 
       setState((prev: any) => ({
         ...prev,
--- frontend/react-app/src/pages/home/components/panelItem/destination/aiRecommendation/chatbot/config.tsx
+++ frontend/react-app/src/pages/home/components/panelItem/destination/aiRecommendation/chatbot/config.tsx
@@ -1,8 +1,11 @@
-import { ChatBotProps } from "@/pages/home/components/panelItem/destination/aiRecommendation/chatbot/ChatBot";
-import ChatBot from "@/pages/home/components/panelItem/destination/aiRecommendation/chatbot/ChatBot";
 import { createChatBotMessage } from "react-chatbot-kit";
+import { ChatBotProps } from "@/pages/home/components/panelItem/destination/aiRecommendation/chatbot/ChatBotPage";
 
-const config: ChatBotProps["config"] = {
+type Props = {
+  header?: React.ReactNode;
+};
+
+const makeChatbotConfig: ({ header }: Props) => ChatBotProps["config"] = ({ header }: Props) => ({
   initialMessages: [
     createChatBotMessage(
       `안녕하세요. 여행 추천 플랫폼 GO And Travel 입니다. 어떤 느낌의 도시를 방문하시고 싶으신가요?`,
@@ -10,9 +13,9 @@ const config: ChatBotProps["config"] = {
     ),
   ],
   customComponents: {
-    header: () => <ChatBot.Header />,
+    header: () => header,
   },
   customStyles: {},
-};
+});
 
-export default config;
+export default makeChatbotConfig;
