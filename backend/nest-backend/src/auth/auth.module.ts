20a336f422085bcbca3278a451c469885827ed35
nuyeo <bluelayzxx@gmail.com>
2024-08-19 22:17:08 +0900

    feat: auth.module에 passportModule import

--- backend/nest-backend/src/auth/auth.module.ts
+++ backend/nest-backend/src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy],
 })
