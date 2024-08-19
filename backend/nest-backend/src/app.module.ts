a22427f92aec480635bedd3f79e77f384f3c5891
nuyeo <bluelayzxx@gmail.com>
2024-08-19 22:36:55 +0900

    chore: users.module 의존성 추가

--- backend/nest-backend/src/app.module.ts
+++ backend/nest-backend/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
-// import { AuthModule } from './auth/auth.module';
-// import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import * as Joi from 'joi';
@@ -33,8 +33,8 @@ import { AppService } from './app.service';
         return typeOrmConfig(configService);
       },
     }),
-    // AuthModule,
-    // UsersModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/nest-backend/src/users/users.module.ts
+++ backend/nest-backend/src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
   providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
