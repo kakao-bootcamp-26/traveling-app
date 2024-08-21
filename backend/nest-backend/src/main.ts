b12f2f030c793e8397e613bca19276f02b2c835f
JongMany <blackberry1114@naver.com>
2024-08-22 00:14:05 +0900

    fix: 머지 후 발생한 에러 수정(의존성 충돌)

--- backend/nest-backend/src/main.ts
+++ backend/nest-backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
+// import * as session from 'express-session';
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
