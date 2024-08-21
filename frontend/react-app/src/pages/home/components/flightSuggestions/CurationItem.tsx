797354f8ae2b8377fe06c673da4e0fe41d2d1a30
JongMany <blackberry1114@naver.com>
2024-08-21 13:34:51 +0900

    style: 가격에 , 붙이도록 스타일 수정

--- frontend/react-app/src/pages/home/components/flightSuggestions/CurationItem.tsx
+++ frontend/react-app/src/pages/home/components/flightSuggestions/CurationItem.tsx
@@ -19,21 +19,28 @@ export default function CurationItem({ curation }: Props) {
       </section>
       <section className="w-[25%] bg-dark-grey px-3 py-4">
         <div className="mb-4 text-[13px]">
-          <div>어른: ₩{curation.fare.adult}</div>
-          <div>어린이: ₩{curation.fare.child}</div>
-          <div>유아: ₩{curation.fare.infant}</div>
+          <div>어른: ₩{curation.fare.adult.toLocaleString("en-US")}</div>
+          <div>어린이: ₩{curation.fare.child.toLocaleString("en-US")}</div>
+          <div>유아: ₩{curation.fare.infant.toLocaleString("en-US")}</div>
         </div>
 
         <div className="mb-2">
-          <div>어른: ${selectedTravelInfo.passenger.count.adults} 명</div>
-          <div>어린이: ${selectedTravelInfo.passenger.count.children} 명</div>
-          <div>유아: ${selectedTravelInfo.passenger.count.children} 명</div>
+          <div className="grid grid-cols-[1fr_1fr]">
+            <span>어른</span>
+            <span>: {selectedTravelInfo.passenger.count.adults} 명</span>
+            <span>어린이</span>
+            <span>: {selectedTravelInfo.passenger.count.children} 명</span>
+            <span>유아</span>
+            <span>: {selectedTravelInfo.passenger.count.infants} 명</span>
+          </div>
           <hr className="my-2" />
           <div>
             총: ₩
-            {curation.fare.adult * selectedTravelInfo.passenger.count.adults +
+            {(
+              curation.fare.adult * selectedTravelInfo.passenger.count.adults +
               curation.fare.child * selectedTravelInfo.passenger.count.children +
-              curation.fare.infant * selectedTravelInfo.passenger.count.infants}
+              curation.fare.infant * selectedTravelInfo.passenger.count.infants
+            )?.toLocaleString("en-US")}
           </div>
         </div>
         <div className="flex justify-center px-2 py-2 font-semibold bg-blue-400 rounded-md">
