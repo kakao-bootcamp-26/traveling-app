e04758d36bcd863748c31f59313e5cf6c0ef9d0a
JongMany <blackberry1114@naver.com>
2024-08-26 16:40:52 +0900

    fix: 구글맵 LoadScript 위치 수정(useShowAirportMap ➡️ MainLayout)

--- frontend/react-app/src/pages/home/hooks/map/useShowAirportMap.tsx
+++ frontend/react-app/src/pages/home/hooks/map/useShowAirportMap.tsx
@@ -1,5 +1,5 @@
 import useGetAirportCoordinate from "@/pages/home/hooks/map/useGetAirportCoordinate";
-import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import { GoogleMap, MarkerF } from "@react-google-maps/api";
 
 export default function useShowAirportMap() {
   const coordinates = useGetAirportCoordinate();
@@ -7,29 +7,29 @@ export default function useShowAirportMap() {
   return (
     <div style={{ width: "100%", height: "20vh", borderRadius: "10px" }}>
       {coordinates && (
-        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
-          <GoogleMap
-            mapContainerStyle={{
-              width: "100%",
-              height: "100%",
-            }}
-            center={{
+        // <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          center={{
+            lat: coordinates.lat,
+            lng: coordinates.lon,
+          }}
+          zoom={14}
+        >
+          <MarkerF
+            position={{
               lat: coordinates.lat,
               lng: coordinates.lon,
             }}
-            zoom={14}
-          >
-            <MarkerF
-              position={{
-                lat: coordinates.lat,
-                lng: coordinates.lon,
-              }}
-              icon={
-                "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-              }
-            ></MarkerF>
-          </GoogleMap>
-        </LoadScript>
+            icon={
+              "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+            }
+          ></MarkerF>
+        </GoogleMap>
+        // </LoadScript>
       )}
     </div>
   );
