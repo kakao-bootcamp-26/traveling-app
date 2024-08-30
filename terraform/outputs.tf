0231a4eb2cee6e3410ef925b7c9c6a9ec47069d6
apaals2 <kkhm97@gmail.com>
2024-08-30 23:07:39 +0900

    fix: 테라폼 아웃풋에서 백엔드, db 프라이빗 ip 출력되게 수정

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,12 +5,12 @@ output "frontend_instance_public_ip" {
   value       = aws_instance.frontend.public_ip
 }
 
-output "backend_instance_public_ip" {
+output "backend_instance_private_ip" {
   description = "Public IP address of the backend instance"
-  value       = aws_instance.backend.public_ip
+  value       = aws_instance.backend.private_ip
 }
 
-output "db_instance_public_ip" {
+output "db_instance_private_ip" {
   description = "Public IP address of the database instance"
-  value       = aws_instance.db.public_ip
+  value       = aws_instance.db.private_ip
 }
