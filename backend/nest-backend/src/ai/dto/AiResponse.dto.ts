53f0d191af2c97230198c7a1a5b0a8c39ef297f6
nuyeo <bluelayzxx@gmail.com>
2024-09-02 11:08:35 +0900

    feat: aiRequestDto, aiResponseDto에 validation 추가

--- backend/nest-backend/src/ai/dto/AiResponse.dto.ts
+++ backend/nest-backend/src/ai/dto/AiResponse.dto.ts
@@ -1,4 +1,8 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class AiResponseDto {
+  @IsString()
+  @IsNotEmpty()
   message: string; // 모델 응답 메시지
   intent_response: string; // 모델 응답의 유형
   recommendation: string; // 추천 여행지
