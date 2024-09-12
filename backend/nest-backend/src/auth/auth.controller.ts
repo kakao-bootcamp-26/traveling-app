a313d95ebef2aa2e235e97deb48821f4a5a95a63
nuyeo <bluelayzxx@gmail.com>
2024-09-12 11:54:49 +0900

    refactor: redirect URL을 configModule을 활용하게 수정

--- backend/nest-backend/src/auth/auth.controller.ts
+++ backend/nest-backend/src/auth/auth.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService, GoogleRequest } from './auth.service';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {}
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
@@ -16,22 +20,13 @@ export class AuthController {
   async googleAuthRedirect(@Req() _req: GoogleRequest, @Res() _res: Response) {
     try {
       const jwt = await this.authService.googleLogin(_req);
-      // return {
-      //   statusCode: 200,
-      //   data: user,
-      //   message: 'User login success',
-      // };
-      // JWT를 쿠키에 저장하고 클라이언트로 리다이렉트
-      _res.cookie('jwt', jwt, { httpOnly: true, secure: false }); // secure: true는 HTTPS에서만 전송
-      return _res.redirect('http://localhost:5173/main'); // 로그인 후 리다이렉트할 프론트엔드 페이지
-    } catch (error) {
-      return _res.redirect('http://localhost:5173/error');
+      _res.cookie('jwt', jwt, { httpOnly: true, secure: false });
 
-      // return {
-      //   statusCode: 500,
-      //   message: 'Google login failed',
-      //   error: 'Internal Server Error',
-      // };
+      const frontAddress = this.configService.get<string>('FRONT_ADDRESS');
+      return _res.redirect(`${frontAddress}/main`);
+    } catch (error) {
+      const frontAddress = this.configService.get<string>('FRONT_ADDRESS');
+      return _res.redirect(`${frontAddress}/error`);
     }
   }
 }
